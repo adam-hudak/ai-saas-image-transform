91b0a9d9560187a2a50e8458fd08d89b8e96b581
adam-hudak <adamhudak1@proton.me>
2024-05-22 21:33:28 +0200

    feat: add sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
+import Sidebar from "@/components/shared/Sidebar";
+
 const Root = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
